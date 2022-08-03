a7f176586b24f4d191ca11d6941ec030b62239f9
lnklkvkn <lnklkvkn@mail.ru>
2022-08-03 14:26:01 +0300

    does not work

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -5,4 +5,35 @@
 //  Created by ln on 02.08.2022.
 //
 
-import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setup()
+    }
+    
+    private func setup() {
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self)?.first as? UIView else {
+            return UIView()
+        }
+        
+        return view
+    }
+}
+
+
+
