879a9c2e715918c86e76db1fca191efd3cf34e52
lnklkvkn <lnklkvkn@mail.ru>
2022-08-03 14:24:13 +0300

    does not work

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by ln on 02.08.2022.
+//
+
+import Foundation
---
+++ Netology_IB_Instruments/ProfileViewController..swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController..swift
+//  Netology_IB_Instruments
+//
+//  Created by ln on 03.08.2022.
+//
+
+import UIKit
+
+class ProfileViewController_: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
